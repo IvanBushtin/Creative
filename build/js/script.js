2eaf7791c028ff71dbadab5b6485420c9dcffd6b
IvanBushtin <ivanbushtin77@gmail.com>
2019-03-13 18:21:02 +0200

    add portfolio section

--- build/js/script.js
+++ build/js/script.js
@@ -10,5 +10,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 264
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+    gutter:30,
+    horizontalOrder: true
+});
+
 
 
